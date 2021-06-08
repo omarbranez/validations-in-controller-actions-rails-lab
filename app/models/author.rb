ef4ef721f782cc2f349a28c2fc19268c577caaef
Omar Branez <omar.branez@gmail.com>
2021-06-08 12:19:17 -0400

    commit tree ec0bb4dd816418c4b63c686e16386787e2272002

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true, allow_blank: false
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, allow_blank: false
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :content, length: { minimum: 100}
 end
